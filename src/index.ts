586e0a9719cf7dee3ebe59d4b7b515d7eb6a1205
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-03-01 21:36:51 +0100

    refactor to use Interfaces

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ import { CustomMap } from './CustomMap';
 const user = new User();
 const company = new Company();
 const map = new CustomMap('map');
-map.addUserMarker(user);
-map.addUserMarker(company);
+map.addMarker(user);
+map.addMarker(company);
