f1b0a84b02286a2e63abfa0ace8d5f6acfd4b735
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-03-01 21:25:34 +0100

    add a marker for a Company

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { User } from './User';
-//import { Company } from './Company';
+import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
 const user = new User();
+const company = new Company();
 const map = new CustomMap('map');
 map.addUserMarker(user);
+map.addUserMarker(company);
